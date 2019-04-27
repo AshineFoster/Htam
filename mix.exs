26283c6448e6d6ee32a8b93decc1e25ce5ff75bd
AshineFoster <ashinefoster@gmail.com>
2019-04-27 17:37:35 -0500

    fixed problems with abiguity for constants pi & e

--- mix.exs
+++ mix.exs
@@ -7,22 +7,35 @@ defmodule Htam.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      build_embedded: Mix.env == :prod,
+      description: description(),
+      package: package(),
+      name: "Htam",
+      source_url: "https://github.com/AshineFoster/Htam"
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false},
+    ]
+  end
+
+  defp description() do
+    "A few sentences (a paragraph) describing the project."
+  end
+
+  defp package() do
+    [
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/AshineFoster/Htam"}
     ]
   end
 end
